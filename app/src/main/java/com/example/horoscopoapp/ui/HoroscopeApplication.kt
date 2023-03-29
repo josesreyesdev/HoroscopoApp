6bc6a1f5654b9f5d877092d0cc6067629f25378b
Jose SR <josesanchez09190@gmail.com>
2023-03-28 19:55:32 -0600

    Add features/retrofit-fragments-hilt

---
+++ app/src/main/java/com/example/horoscopoapp/ui/HoroscopeApplication.kt
@@ -0,0 +1,8 @@
+package com.example.horoscopoapp.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HoroscopeApplication: Application() {
+}
---
+++ app/src/main/java/com/example/horoscopoapp/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.horoscopoapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscopoapp.R
+import com.example.horoscopoapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController : NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
