979009858fd941798d7e84495482c9b4afe1b61e
Jose SR <josesanchez09190@gmail.com>
2023-04-17 22:59:29 -0600

    create RandomCardsProvider and load images

--- app/src/main/java/com/example/horoscopoapp/ui/lucky/LuckyFragment.kt
+++ app/src/main/java/com/example/horoscopoapp/ui/lucky/LuckyFragment.kt
@@ -1,22 +1,35 @@
 package com.example.horoscopoapp.ui.lucky
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import com.example.horoscopoapp.R
 import com.example.horoscopoapp.databinding.FragmentLuckyBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.random.Random
+import javax.inject.Inject
+
+const val TAG = "LuckyFragment"
 
 @AndroidEntryPoint
 class LuckyFragment : Fragment() {
 
     private val viewModel by viewModels<LuckyViewModel>()
-    private var _binding : FragmentLuckyBinding? = null
+    private var _binding: FragmentLuckyBinding? = null
     private val binding get() = _binding!!
+
+    @Inject
+    lateinit var randomCardsProvider: RandomCardsProvider
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,22 +43,70 @@ class LuckyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val rotationCre: Animation = AnimationUtils.loadAnimation(requireContext(), R.anim.rotation)
         binding.apply {
+            /*tvLuckyInfo.startAnimation(rotationCre)
+            viewFrontContainer.ivLuckyCard */
 
+            viewBackContainer.viewBack.setOnClickListener {
+                prepareCard()
+                flipCard()
+            }
+        }
+    }
+
+    private fun flipCard() {
+        try {
+            //Visibilidad
+            binding.viewFrontContainer.viewFront.isVisible = true
+
+            //Efecto 3D
+            val scale = requireContext().resources.displayMetrics.density
+            val cameraDist = 8000 * scale
+            binding.viewFrontContainer.viewFront.cameraDistance = cameraDist
+            binding.viewBackContainer.viewBack.cameraDistance = cameraDist
+
+            //Recuperamos animacion FLIP OUT
+            val flipOutAnimatorSet = AnimatorInflater.loadAnimator(
+                requireContext(), R.animator.flip_out
+            ) as AnimatorSet
+
+            flipOutAnimatorSet.setTarget(binding.viewBackContainer.viewBack)
+
+            //Recuperamos y seteamos la animación FLIP IN
+            val flipInAnimatorSet =
+                AnimatorInflater.loadAnimator(requireContext(), R.animator.flip_in) as AnimatorSet
+            flipInAnimatorSet.setTarget(binding.viewFrontContainer.viewFront)
+
+
+//            flipInAnimatorSet.doOnStart {
+//                binding.tvLuckyInfo.animate().alpha(1f).duration = 1500
+//            }
+
+            //Iniciamos las animaciones
+            flipOutAnimatorSet.start()
+            flipInAnimatorSet.start()
+
+            flipInAnimatorSet.doOnEnd {
+                binding.tvLuckyInfo.animate().alpha(1f).duration = 1500
+                binding.viewBackContainer.viewBack.isVisible = false
+            }
+
+        } catch (e: Exception) {
+            Log.e(TAG, "Error FlipCard() : ${e.message}")
         }
     }
 
     private fun prepareCard() {
-        val image = when (Random.nextInt(0, 5)) {
-            0 -> R.drawable.ic_compatibility
-            1 -> R.drawable.ic_compatibility
-            2 -> R.drawable.ic_compatibility
-            3 -> R.drawable.ic_compatibility
-            4 -> R.drawable.ic_compatibility
-            5 -> R.drawable.ic_compatibility
-            else -> R.drawable.ic_compatibility
-        }
+        val luck = randomCardsProvider.getLucky()
 
+        binding.apply {
+            viewFrontContainer.ivLuckyCard.setImageDrawable(
+                ContextCompat.getDrawable(requireContext(), luck.image)
+            )
+
+            tvLuckyInfo.text = getString(luck.text)
+        }
     }
 
 }
