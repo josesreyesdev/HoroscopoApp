b39c4b6a6d3d2d5654400f911113f392c657b433
Jose SR <josesanchez09190@gmail.com>
2023-04-18 18:16:03 -0600

    First commit retrofit

--- app/src/main/java/com/example/horoscopoapp/core/NetworkModule.kt
+++ app/src/main/java/com/example/horoscopoapp/core/NetworkModule.kt
@@ -7,6 +7,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -20,6 +22,14 @@ private val moshi = Moshi.Builder()
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
 
     @Provides
     @Singleton
